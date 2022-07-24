6f85b6b6287b2c918e65df089aa020c0661f5979
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2022-07-24 18:08:07 -0400

    Test#1 added

--- cypress/integration/TestReactShoppingCart.js
+++ cypress/integration/TestReactShoppingCart.js
@@ -1,10 +1,70 @@
-/// <reference types="Cypress" />"
+/// <reference types="Cypress" />
 
-import reactShoppingCartPage from '../../support/POM/ReactShoppingCart'
+import ReactShoppingCartPage from '../support/POM/ReactShoppingCart'
 
 describe('Reat Shopping Cart Test Suite', function () {
 
-	it('1st TC', function () {
+	it('Test#1 - Verify that user can sort items by size', function () {
+		cy.visit('https://react-shopping-cart-67954.firebaseapp.com/');
 
+		// AJAX with products data
+		cy.intercept({
+			method: 'GET',
+			url: 'https://react-shopping-cart-67954.firebaseio.com/products.json',
+		}).as('getData');
+
+		// React Shopping Cart page objects
+		const rsc = new ReactShoppingCartPage();
+
+		// verify XS size - should be equal 1
+		rsc.getXSSize().click();
+		cy.wait('@getData')
+		rsc.getProductContainers().should('have.length', rsc.xs);
+		rsc.getXSSize().click();
+		cy.wait('@getData')
+
+
+		// verify S size - should be equal 2
+		rsc.getSSize().click();
+		cy.wait('@getData');
+		rsc.getProductContainers().should('have.length', rsc.s);
+		rsc.getSSize().click();
+		cy.wait('@getData');
+
+
+		// // verify M size - should be equal 1
+		rsc.getMSize().click();
+		cy.wait('@getData');
+		rsc.getProductContainers().should('have.length', rsc.m);
+		rsc.getMSize().click();
+		cy.wait('@getData');
+
+		// // verify ML size - should be equal 2
+		rsc.getMLSize().click();
+		cy.wait('@getData');
+		rsc.getProductContainers().should('have.length', rsc.ml);
+		rsc.getMLSize().click();
+		cy.wait('@getData');
+
+		// verify L size - should be equal 10
+		rsc.getLSize().click();
+		cy.wait('@getData');
+		rsc.getProductContainers().should('have.length', rsc.l);
+		rsc.getLSize().click();
+		cy.wait('@getData');
+
+		// verify XL size - should be equal 10
+		rsc.getXLSize().click();
+		cy.wait('@getData');
+		rsc.getProductContainers().should('have.length', rsc.xl);
+		rsc.getXLSize().click();
+		cy.wait('@getData');
+
+		// verify XXL size - should be equal 4
+		rsc.getXXLSize().click();
+		cy.wait('@getData');
+		rsc.getProductContainers().should('have.length', rsc.xxl);
+		rsc.getXXLSize().click();
+		cy.wait('@getData');
 	});
 })
--- cypress/support/POM/ReactShoppingCart.js
+++ cypress/support/POM/ReactShoppingCart.js
@@ -1,5 +1,124 @@
-class ReactShoppingCart {
+/// <reference types="Cypress" />"
+/// <reference types="cypress-xpath" />
 
+const xsSize = "input[type=checkbox][value='XS']";
+const sSize = "input[type=checkbox][value='S']";
+const mSize = "input[type=checkbox][value='M']";
+const mlSize = "input[type=checkbox][value='ML']";
+const lSize = "input[type=checkbox][value='L']";
+const xlSize = "input[type=checkbox][value='XL']";
+const xxlSize = "input[type=checkbox][value='XXL']";
+const parentSize = "label";
+const xs = 1;
+const s = 2;
+const m = 1;
+const ml = 2;
+const l = 10;
+const xl = 10;
+const xxl = 4;
+const cartBtn = "//div[@title='Products in cart quantity']/parent::div/parent::button";
+const productsInCart = "//div[@title='Products in cart quantity']";
+const github = "a[href*=github]";
+const checkoutBtn = "//button[text()='Checkout']";
+const subtotal = "//p[text()='SUBTOTAL']/following-sibling::div/p[1]";
+const splitSubtotal = "//p[text()='SUBTOTAL']/following-sibling::div/p[2]";
+const productContainers = ".sc-124al1g-2";
+const addToCart = "Add to cart";
+const shippingLabel = "//div[text()='Free shipping']";
+const plus = "//button[text()='-']";
+const minus = "//button[text()='+']";
+const cartProductPrice = ".sc-11uohgb-4 p";
+const cartItems = ".sc-11uohgb-0";
+
+class ReactShoppingCart {
+    constructor(){
+		this.xs = xs;
+		this.s = s;
+		this.m = m;
+		this.ml = ml;
+		this.l = l;
+		this.xl = xl;
+		this.xxl = xxl;
+    }
+	getGitHub() {
+		return cy.get(github);
+	}
+
+	getXSSize() {
+		return cy.get(xsSize, { timeout: 1000 }).parent(parentSize);
+	}
+
+	getSSize() {
+		return cy.get(sSize, { timeout: 1000 }).parent(parentSize);
+	}
+
+	getMSize() {
+		return cy.get(mSize, { timeout: 1000 }).parent(parentSize);
+	}
+
+	getMLSize() {
+		return cy.get(mlSize, { timeout: 1000 }).parent(parentSize);
+	}
+
+	getLSize() {
+		return cy.get(lSize, { timeout: 1000 }).parent(parentSize);
+	}
+
+	getXLSize() {
+		return cy.get(xlSize, { timeout: 1000 }).parent(parentSize);
+	}
+
+	getXXLSize() {
+		return cy.get(xxlSize, { timeout: 1000 }).parent(parentSize);
+	}
+
+	getCartBtn() {
+		return cy.xpath(cartBtn);
+	}
+
+	getProductsInCart() {
+		return cy.xpath(productsInCart);
+	}
+
+	getCheckoutBtn() {
+		return cy.xpath(checkoutBtn);
+	}
+
+	getSubtotal() {
+		return cy.xpath(subtotal);
+	}
+
+	getSplitSubtotal() {
+		return cy.xpath(splitSubtotal);
+	}
+
+	getProductContainers() {
+		return cy.get(productContainers, { timeout: 2000 });
+	}
+
+	getAddToCart(index) {
+		return this.getProductContainers().eq(index).find(addToCart).eq(index);
+	}
+
+	getShippingLabel(index) {
+		return this.getProductContainers().eq(index).find(shippingLabel).eq(index);
+	}
+
+	getCartItems() {
+		return cy.get(cartItems);
+	}
+
+	getMinus() {
+		return cy.xpath(minus);
+	}
+
+	getPlus() {
+		return cy.xpath(plus);
+	}
+
+	getCartProductPrice() {
+		return cy.get(cartProductPrice);
+	}
 }
 
 export default ReactShoppingCart;
