9a2e30798606f7c41337b57ae3d9454d1f10eb6e
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2022-07-24 13:34:28 -0400

    cypress framework created and configured

---
+++ cypress/integration/TestReactShoppingCart.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />"
+
+import reactShoppingCartPage from '../../support/POM/ReactShoppingCart'
+
+describe('Reat Shopping Cart Test Suite', function () {
+
+	it('1st TC', function () {
+
+	});
+})
---
+++ cypress/support/POM/ReactShoppingCart.js
@@ -0,0 +1,5 @@
+class ReactShoppingCart {
+
+}
+
+export default ReactShoppingCart;
