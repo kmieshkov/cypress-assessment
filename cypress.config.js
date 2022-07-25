10d9483ec45bbfdc3c85483082f788825484e1fb
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2022-07-25 12:28:56 -0400

    project configured

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,17 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: 'cypress/integration/*.js'
+    specPattern: 'cypress/integration/*/*.js',
+    defaultCommandTimeout: 5000,
+    pageLoadTimeout: 15000,
+    env: {
+      shoppingCart: 'https://react-shopping-cart-67954.firebaseapp.com/',
+      demoqa: 'https://demoqa.com/frames'
+    },
+    reporter: 'mochawesome',
+    // how many times it will run failed test before throw a failure
+    retries: {
+      runMode: 2
+    }
   },
 });
